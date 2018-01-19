aed3bfa947fc41bf9912e356b6bc9a1d312cd3f1
Daniel Baah <danielduhh@gmail.com>
2018-01-18 16:38:40 -0800

    initial commit. flags multi-featured roundabouts

---
+++ src/main/java/org/openstreetmap/atlas/checks/validation/MultiFeatureRoundaboutCheck.java
@@ -0,0 +1,134 @@
+package org.openstreetmap.atlas.checks.validation;
+
+import java.util.*;
+
+import org.openstreetmap.atlas.checks.base.BaseCheck;
+import org.openstreetmap.atlas.checks.flag.CheckFlag;
+import org.openstreetmap.atlas.geography.atlas.items.*;
+import org.openstreetmap.atlas.tags.HighwayTag;
+import org.openstreetmap.atlas.tags.JunctionTag;
+import org.openstreetmap.atlas.utilities.configuration.Configuration;
+
+/**
+ * This checks flags multi-featured roundabouts. According to OSM Wiki, each round about should be exactly one feature (OSM Way).
+ * We can create the WrongWayRoundaboutCheck based on this exact principle.
+ *
+ * @author danielbaah
+ */
+public class MultiFeatureRoundaboutCheck extends BaseCheck {
+
+    // You can use serialver to regenerate the serial UID.
+    private static final long serialVersionUID = 1L;
+    private static final List<String> FALLBACK_INSTRUCTIONS = Arrays
+            .asList("Edge: {0} is a part of a multi-feature roundabout. Roundabout Edges: {1}");
+    // Highway tags referring roundabouts
+    private static final EnumSet<HighwayTag> HIGHWAY_TAGS_FOR_ROUNDABOUTS = EnumSet
+            .of(HighwayTag.MINI_ROUNDABOUT);
+
+    @Override
+    protected List<String> getFallbackInstructions() {
+        return FALLBACK_INSTRUCTIONS;
+    }
+
+    /**
+     * The default constructor that must be supplied. The Atlas Checks framework will generate the
+     * checks with this constructor, supplying a configuration that can be used to adjust any
+     * parameters that the check uses during operation.
+     *
+     * @param configuration the JSON configuration for this check
+     */
+    public MultiFeatureRoundaboutCheck(final Configuration configuration) {
+        super(configuration);
+    }
+
+    /**
+     * This function will validate if the supplied atlas object is valid for the check.
+     *
+     * @param object the atlas object supplied by the Atlas-Checks framework for evaluation
+     * @return {@code true} if this object should be checked
+     */
+    @Override
+    public boolean validCheckForObject(final AtlasObject object) {
+
+        // by default we will assume all objects as valid
+        return object instanceof Edge
+                // check if already marked flagged
+                && !this.isFlagged(object.getOsmIdentifier())
+                // check junction=roundabout tag
+                && JunctionTag.isRoundabout(object);
+                // TODO check for highway=mini_roundabout
+
+    }
+
+    /**
+     * This is the actual function that will check to see whether the object needs to be flagged.
+     *
+     * @param object the atlas object supplied by the Atlas-Checks framework for evaluation
+     * @return an optional {@link CheckFlag} object that
+     */
+    @Override
+    protected Optional<CheckFlag> flag(final AtlasObject object)
+    {
+        final Edge edge = (Edge) object;
+        // array list of osm unique identifiers
+        final List<Long> roundaboutEdgeOsmIds = new ArrayList<>();
+
+        // append roundabout ids to roundaboutEdgeOsmIds
+        traverseRoundabout(edge, roundaboutEdgeOsmIds);
+
+        // Rule: If edges with junction=roundabout tag have the same id, do not flag. This indicates a one feature roundabout.
+        if (roundaboutEdgeOsmIds.size() == 0)
+        {
+            return Optional.empty();
+        }
+        else
+        {
+            // mark as flagged
+            this.markAsFlagged(object.getOsmIdentifier());
+            // create flag w/ instructions
+            return Optional.of(
+                    this.createFlag(object, this.getLocalizedInstruction(
+                            0,
+                            object.getOsmIdentifier(),
+                            roundaboutEdgeOsmIds.toString())));
+            }
+    }
+
+    /**
+     * Recursively loop through connected edges & append osmIds to our list of edges with junction=roundabout
+     * @param edge
+     *      current ({@link Edge}) {@link AtlasObject}
+     * @param osmIds
+     *      list of osmIds in single roundabout
+     */
+    private void traverseRoundabout(Edge edge, List<Long> osmIds) {
+        Iterator<Edge> r = edge.connectedEdges().iterator();
+
+        // iterate through connectedEdges
+        while (r.hasNext())
+        {
+            final Edge roundabout = r.next();
+            final Long osmId = roundabout.getOsmIdentifier();
+            // grab all edges with roundabout tag
+            if (JunctionTag.isRoundabout(roundabout))
+            {
+                // make sure our list doesn't already contain the id
+                if (!osmIds.contains(osmId)
+                        // or the AtlasObject id
+                        && osmId != edge.getOsmIdentifier()
+                        // this flag removes duplicate challenges
+                        // TODO determine if we want each Edge in roundabout to be its own challenge
+                        && !this.isFlagged(roundabout.getOsmIdentifier()))
+                {
+                    // mark as flagged
+                    this.markAsFlagged(roundabout.getOsmIdentifier());
+                    // add to list of roundabout unique ids
+                    osmIds.add(roundabout.getOsmIdentifier());
+                    // check for more edges
+                    traverseRoundabout(roundabout, osmIds);
+                }
+            }
+        }
+
+    }
+}
