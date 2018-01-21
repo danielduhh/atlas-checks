ce0d9928521f0b7e5984f35d8e6e58661a3a1d31
Daniel Baah <danielduhh@gmail.com>
2018-01-20 16:36:05 -0800

    flag collection of AtlasObjects replace getOsmIdentifier w/ getIdentifier for truly unique value

--- src/main/java/org/openstreetmap/atlas/checks/validation/MultiFeatureRoundaboutCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/MultiFeatureRoundaboutCheck.java
@@ -7,6 +7,7 @@ import org.openstreetmap.atlas.checks.flag.CheckFlag;
 import org.openstreetmap.atlas.geography.atlas.items.*;
 import org.openstreetmap.atlas.tags.HighwayTag;
 import org.openstreetmap.atlas.tags.JunctionTag;
+import org.openstreetmap.atlas.tags.annotations.validation.Validators;
 import org.openstreetmap.atlas.utilities.configuration.Configuration;
 
 /**
@@ -17,13 +18,10 @@ import org.openstreetmap.atlas.utilities.configuration.Configuration;
  */
 public class MultiFeatureRoundaboutCheck extends BaseCheck {
 
-    // You can use serialver to regenerate the serial UID.
+    // TODO You can use serialver to regenerate the serial UID.
     private static final long serialVersionUID = 1L;
     private static final List<String> FALLBACK_INSTRUCTIONS = Arrays
-            .asList("Edge: {0} is a part of a multi-feature roundabout. Roundabout Edges: {1}");
-    // Highway tags referring roundabouts
-    private static final EnumSet<HighwayTag> HIGHWAY_TAGS_FOR_ROUNDABOUTS = EnumSet
-            .of(HighwayTag.MINI_ROUNDABOUT);
+            .asList("This is a multi-feature roundabout. Merge roundabout edges to create one feature (OSM Way).");
 
     @Override
     protected List<String> getFallbackInstructions() {
@@ -50,14 +48,14 @@ public class MultiFeatureRoundaboutCheck extends BaseCheck {
     @Override
     public boolean validCheckForObject(final AtlasObject object) {
 
+        final Optional<HighwayTag> highwayTag = Validators.from(HighwayTag.class, object);
+
         // by default we will assume all objects as valid
         return object instanceof Edge
                 // check if already marked flagged
-                && !this.isFlagged(object.getOsmIdentifier())
+                && !this.isFlagged(object.getIdentifier())
                 // check junction=roundabout tag
                 && JunctionTag.isRoundabout(object);
-                // TODO check for highway=mini_roundabout
-
     }
 
     /**
@@ -71,61 +69,59 @@ public class MultiFeatureRoundaboutCheck extends BaseCheck {
     {
         final Edge edge = (Edge) object;
         // array list of osm unique identifiers
-        final List<Long> roundaboutEdgeOsmIds = new ArrayList<>();
+        final Set<AtlasObject> roundaboutEdges = new HashSet<>();
 
-        // append roundabout ids to roundaboutEdgeOsmIds
-        traverseRoundabout(edge, roundaboutEdgeOsmIds);
+        // loop through connected edges with junction=roundabout tags
+        stitchRoundaboutEdges(edge, roundaboutEdges);
 
-        // Rule: If edges with junction=roundabout tag have the same id, do not flag. This indicates a one feature roundabout.
-        if (roundaboutEdgeOsmIds.size() == 0)
-        {
+        // Rule: If edges with junction=roundabout tag have the same id, do not flag.
+        // This indicates a one feature roundabout.
+        if (roundaboutEdges.size() == 0) {
             return Optional.empty();
-        }
-        else
-        {
+        } else {
             // mark as flagged
-            this.markAsFlagged(object.getOsmIdentifier());
+            this.markAsFlagged(object.getIdentifier());
             // create flag w/ instructions
+            //TODO combine roundabout edges and pass in as Set
             return Optional.of(
-                    this.createFlag(object, this.getLocalizedInstruction(
-                            0,
-                            object.getOsmIdentifier(),
-                            roundaboutEdgeOsmIds.toString())));
+                    this.createFlag(roundaboutEdges, this.getLocalizedInstruction(0)));
             }
-    }
+
+        }
 
     /**
      * Recursively loop through connected edges & append osmIds to our list of edges with junction=roundabout
      * @param edge
      *      current ({@link Edge}) {@link AtlasObject}
-     * @param osmIds
+     * @param roundaboutEdges
      *      list of osmIds in single roundabout
      */
-    private void traverseRoundabout(Edge edge, List<Long> osmIds) {
+    //TODO find a better name for this
+    private void stitchRoundaboutEdges(Edge edge, Set<AtlasObject> roundaboutEdges) {
         Iterator<Edge> r = edge.connectedEdges().iterator();
 
         // iterate through connectedEdges
         while (r.hasNext())
         {
-            final Edge roundabout = r.next();
-            final Long osmId = roundabout.getOsmIdentifier();
+            final Edge connectedEdge = r.next();
+            final Long uniqueId = connectedEdge.getIdentifier();
+
             // grab all edges with roundabout tag
-            if (JunctionTag.isRoundabout(roundabout))
+            if (JunctionTag.isRoundabout(connectedEdge))
             {
                 // make sure our list doesn't already contain the id
-                if (!osmIds.contains(osmId)
+                if (!roundaboutEdges.contains(connectedEdge)
                         // or the AtlasObject id
-                        && osmId != edge.getOsmIdentifier()
+                        && uniqueId != edge.getIdentifier()
                         // this flag removes duplicate challenges
-                        // TODO determine if we want each Edge in roundabout to be its own challenge
-                        && !this.isFlagged(roundabout.getOsmIdentifier()))
+                        && !this.isFlagged(connectedEdge.getIdentifier()))
                 {
                     // mark as flagged
-                    this.markAsFlagged(roundabout.getOsmIdentifier());
+                    this.markAsFlagged(connectedEdge.getIdentifier());
                     // add to list of roundabout unique ids
-                    osmIds.add(roundabout.getOsmIdentifier());
+                    roundaboutEdges.add(connectedEdge);
                     // check for more edges
-                    traverseRoundabout(roundabout, osmIds);
+                    stitchRoundaboutEdges(connectedEdge, roundaboutEdges);
                 }
             }
         }
